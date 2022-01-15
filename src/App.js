7acef151609cb94e2c2cdbeb25a7ccd17d26e4e9
kirakira0 <kiratoal0@gmail.com>
2022-01-15 15:42:53 -0800

    Set up form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const initialValues = {url: ""}
+  const [formValues, setFormValues] = useState(initialValues)
+
+  const onSubmit = (e) =>
+    alert(e.label)
+    // console.log(e)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to YouTube comment section analysis 
+        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +28,23 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+      <div className='container'>
+        <form>
+          <h1>URL Form</h1>
+          <div className='ui-form'>
+            <label>
+              URL: <input type="text" url="url" placeholder='Video URL' value={formValues.url} />
+            </label>
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+
+      </div>
+      <footer>
+        <p>footer</p>
+      </footer>
     </div>
   );
 }
