8a512dd8ebe4670b91c574246d70e87277b148f1
Kimberly K <kimberly.n.kubo@gmail.com>
2022-01-16 09:53:28 -0800

    added padding on navbar

--- src/components/LeftNavbar.js
+++ src/components/LeftNavbar.js
@@ -1,16 +1,18 @@
 import { style } from '@mui/system';
 import { type } from '@testing-library/user-event/dist/type';
 import React, {useState} from 'react';
+import { Navbar } from 'react-bootstrap';
 
 // class LeftNavbar extends React.Component {
 const LeftNavbar = (props) => {
   return (
-    <div className="leftNavbar" style={{width: props.width}}>
+    <Navbar className="leftNavbar">
+      <h4>Youtube comment section analysis</h4>
       <a href="#">Home</a>
       <a href="#">About</a>
       <a href="#">FAQ</a>
       <a href="#">Contact</a>
-    </div>
+    </Navbar>
   );
 };
 export default LeftNavbar;
