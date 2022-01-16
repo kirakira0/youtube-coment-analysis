c1c0aa13a84381571d37f514f37890bba3afeb24
kirakira0 <kiratoal0@gmail.com>
2022-01-16 10:46:21 -0800

    Saving styling progress:

--- src/components/LeftNavbar.js
+++ src/components/LeftNavbar.js
@@ -6,10 +6,14 @@ import React, {useState} from 'react';
 const LeftNavbar = (props) => {
   return (
     <div className="leftNavbar" style={{width: props.width}}>
-      <a href="#">Home</a>
-      <a href="#">About</a>
-      <a href="#">FAQ</a>
-      <a href="#">Contact</a>
+      <h1>SentiCom</h1>
+      <p className='paragraph'>The internet can be a beautiful place, but it can also be a breeding ground for bullying and harassment, particularly towards women, people of color, and other marginalized groups. SentiCom is a webapp which utilizes the power of Google’s Perspective API to perform sentiment analysis on the comments section of any YouTube video. We aim to provide a service through which socioanthropological and psychological researchers (or anyone interested in the power of the language we use) may easily glean useful insights on how online harassment affects content creators of different identities and demographics.</p>
+      <p>Learn more about the impact of online harrassment through the following peer-reviewed, scientific articles.</p>
+      <a href="https://journals.sagepub.com/doi/full/10.1177/0894439319865518" target="_blank">Silencing Women? Gender and Online Harassment by Marjan Nadim and Audun Fladmoe (2021)</a>
+      <p></p>
+      <a href="https://journals.sagepub.com/doi/full/10.1177/1461444819887141" target="_blank">Sluts and soyboys: MGTOW and the production of misogynistic online harassment by Callum Jones, Verity Trott, and Scott Wright (2019)</a>
+      <p></p>
+      <a href="https://www.pewresearch.org/internet/wp-content/uploads/sites/9/2021/01/PI_2021.01.13_Online-Harassment_FINAL.pdf" target="_blank">The State of Online Harassment by Emily A. Vogels (2021)</a>
     </div>
   );
 };
